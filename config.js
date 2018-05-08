42d0700409b31fde62b32ce4f32e212755148cd3
harsh <harshit_agrawal36@yahoo.com>
2018-05-08 14:38:46 -0700

    initial config

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAd0MH67YZCoBALyT6jZBU0yojuIZBCaLttfjEr2qZClIKHAEEaErmyURYaaxYGVHyScaH25v9TZAZAinXHWMnUBJWl74nlZBldgHFxRqgXxVzIUO0SH4Gxh78KgHtyZBJDNf5vJfZClOihGZA8IV8Y2HMwSZA1ZCTl644zt2RQ0vm3ep8TsZChzZAeUzd',
+    FB_VERIFY_TOKEN: 'sdfghjkloiuytrewqasxcvbh32123456765343sxcvbgnhrterwesxcv',
+    API_AI_CLIENT_ACCESS_TOKEN: '8de400f5a7e24ceeb41ba1f538433882',
+    FB_APP_SECRET: '38eca1d19df098fe322e3bff4b7f7dc1',
+    SERVER_URL: "https://chatbot-fb-app.herokuapp.com/",
+};
